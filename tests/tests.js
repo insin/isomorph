cb4fb38a23754749c2fd6552541b47a99ec8cc58
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-12-17 03:04:25 +0000

    [test] Made QUnit tests independent of your cwd; can now test with `npm test`

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,11 @@
 var qunit = require('qunit')
+  , path = require('path')
 
-qunit.run({ code: '../index.js'
-          , tests: [ './is.js'
-                   , './format.js'
-                   , './func.js'
-                   , './object.js'
-                   , './re.js'
+qunit.run({ code: path.join(__dirname, '../index.js')
+          , tests: [ path.join(__dirname, 'is.js')
+                   , path.join(__dirname, 'format.js')
+                   , path.join(__dirname, 'func.js')
+                   , path.join(__dirname, 'object.js')
+                   , path.join(__dirname, 're.js')
                    ]
           })
