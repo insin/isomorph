194a008e94486370c2562db6adcea958ac32dab7
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2012-01-20 00:40:53 +0000

    0.1.5 - extracted isomorph.querystring from sacrum

--- isomorph.js
+++ isomorph.js
@@ -1,5 +1,5 @@
 /**
- * isomorph 0.1.4 - https://github.com/insin/isomorph
+ * isomorph 0.1.5 - https://github.com/insin/isomorph
  * MIT Licensed
  */
 ;(function() {
@@ -286,8 +286,65 @@ module.exports = {
 }
 })
 
+require.define("./querystring", function(module, exports, require) {
+var is = require('./is')
+
+/**
+ * Creates an Object from a query string, providing values for names which are
+ * present more than once as an Array.
+ */
+function parse(query) {
+  var obj = {}
+  if (query.length < 2) {
+    return obj
+  }
+  var params = query.substring(1).split('&')
+  for (var i = 0, l = params.length; i < l; i++) {
+    var parts = params[i].split('=')
+      , name = parts[0]
+      , value = decodeURIComponent(parts[1])
+    if (obj.hasOwnProperty(name)) {
+      if (is.Array(obj[name])) {
+        obj[name].push(value)
+      }
+      else {
+        obj[name] = [obj[name], value]
+      }
+    }
+    else {
+      obj[name] = value
+    }
+  }
+  return obj
+}
+
+/**
+ * Creates a query string from an Object, expecting names with multiple values
+ * to be specified as an Array.
+ */
+function stringify(obj) {
+  var params = []
+  for (var name in obj) {
+    if (is.Array(obj[name])) {
+      for (var a = obj[name], i = 0, l = a.length; i < l; i++) {
+        params.push(name + '=' + encodeURIComponent(a[i]))
+      }
+    }
+    else {
+      params.push(name + '=' + encodeURIComponent(obj[name]))
+    }
+  }
+  return params.join('&')
+}
+
+module.exports = {
+  parse: parse
+, stringify: stringify
+}
+})
+
 require.define("isomorph", function(module, exports, require) {
-exports.version = '0.1.3'
+exports.version = '0.1.5'
 
 exports.is = require('./is')
 exports.array = require('./array')
@@ -295,6 +352,7 @@ exports.func = require('./func')
 exports.object = require('./object')
 exports.format = require('./format')
 exports.re = require('./re')
+exports.querystring = require('./querystring')
 })
 
 window['isomorph'] = require('isomorph')
